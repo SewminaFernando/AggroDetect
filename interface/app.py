1cc703e264b8144863d436bb9817a9f6f7e23e91
Chulanjana <chulanjanajayasinghe2001@gmail.com>
2024-03-15 23:54:10 +0530

    implementing the  program with flask to run in CMD

--- interface/app.py
+++ interface/app.py
@@ -2,26 +2,21 @@ from flask import Flask, render_template, request
 import requests
 import pyttsx3
 from save_report import firebase_datastore
-from model_pipeline import record_and_transcribe
+from model_pipeline import record_and_transcribe, agg_by_voice, agg_by_text, department_by_text
+import subprocess
+import time
 
 app = Flask(__name__)
 
 rasa_server_url = "http://localhost:5005/webhooks/rest/webhook"  # Adjust the URL as needed
 welcome_message_spoken = False
 
-import subprocess
-import time
 
 rasa_shell_process = subprocess.Popen(["rasa", "run"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Wait for the Rasa shell to start up
 time.sleep(45)
-#
-# # Start the Rasa action server in another terminal window
-# rasa_actions_process = subprocess.Popen(["rasa", "run", "actions"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#
-# # Wait for the Rasa actions server to start up
-# time.sleep(5)
+
 
 old_conv = {
     "user_messagee": [],
@@ -45,11 +40,15 @@ def add_welcome_message():
 def chat():
     add_welcome_message()
     audio, user_message = record_and_transcribe()
-
+    agg_voice = agg_by_voice(audio)
+    agg_text = agg_by_text(user_message)
+    dep_text = department_by_text(user_message)
+    print("User Message:", user_message, "\nEmotion audio:", agg_voice, "\nEmotion text:", agg_text, "\nDepartment:",
+          dep_text)
     old_conv['user_message'].append((user_message, "(aggressive level by voice: non angry)", "(aggressive level by "
                                                                                              "text:non angry)"))
-    rasa_response, intent = send_message_to_rasa(user_message)
-    print(rasa_response, intent)
+    rasa_response = send_message_to_rasa(user_message)
+
     # Check if the response from Rasa is not empty and contains 'text'
     if rasa_response and isinstance(rasa_response, list):
         if 'custom' in rasa_response[0]:
@@ -60,13 +59,14 @@ def chat():
             text_speech = pyttsx3.init()
             text_speech.say(bot_response)
             text_speech.runAndWait()
+            print(bot_response)
 
             # Append the bot's response to the conversation
             old_conv['resp'].append(bot_response)
 
             firebase_datastore('unknown', old_conv, department="department")  # put here department
-            render_template('index.html', user_message=(user_message, "(aggressive level by voice: non angry)", "(aggressive level by text:non angry)"), bot_response=bot_response,
-                            old_conv=old_conv, size=len(old_conv["user_messagee"]))
+            # render_template('index.html', user_message=(user_message, "(aggressive level by voice: non angry)", "(aggressive level by text:non angry)"), bot_response=bot_response,
+            #                 old_conv=old_conv, size=len(old_conv["user_messagee"]))
 
         else:
             bot_response = rasa_response[0]['text']
@@ -76,19 +76,21 @@ def chat():
             text_speech.runAndWait()
             # Append the bot's response to the conversation
             old_conv['resp'].append(bot_response)
-            render_template('index.html', user_message=(user_message, "(aggressive level by voice: non angry)", "(aggressive level by text:non angry)"), bot_response=bot_response,
-                            old_conv=old_conv, size=len(old_conv["user_messagee"]))
+            # render_template('index.html', user_message=(user_message, "(aggressive level by voice: non angry)", "(aggressive level by text:non angry)"), bot_response=bot_response,
+            #                 old_conv=old_conv, size=len(old_conv["user_messagee"]))
+            print(bot_response)
             chat()
 
 
     else:
-        bot_response = "sorry, I didn't get that. Can you please repeat?"
+        bot_response="sorry, I didn't get that. Can you please repeat?"
         old_conv['resp'].append(bot_response)
         text_speech = pyttsx3.init()
         text_speech.say(bot_response)
         text_speech.runAndWait()
-        render_template('index.html', user_message=(user_message, "(aggressive level by voice: non angry)", "(aggressive level by text:non angry)"), bot_response="No Response",
-                        old_conv=old_conv, size=len(old_conv["user_messagee"]))
+        # render_template('index.html', user_message=user_message, bot_response="No Response",
+        #                         old_conv=old_conv, size=len(old_conv["user_messagee"]))
+        print(bot_response)
         chat()
 
 
@@ -106,3 +108,4 @@ def send_message_to_rasa(message):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    chat()
