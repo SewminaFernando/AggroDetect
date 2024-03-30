0c4d1a947084ef56b2d2edb2ddcc658b8657aec0
PehansiK <pehansiranumi@gmail.com>
2024-03-30 12:17:35 +0530

    Connecting Chat Interface with the Dashboard

--- interface/static/Js/main.js
+++ interface/static/Js/main.js
@@ -19,3 +19,31 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  const chatbotButton = document.getElementById('chatbotButton');
+  const chatContainer = document.getElementById('chatContainer');
+
+  chatbotButton.addEventListener('click', function () {
+      if (chatContainer.style.display === 'none') {
+          chatContainer.style.display = 'block';
+          chatbotButton.innerHTML = '<i class="bx bx-phone-off"></i>';
+      } else {
+          chatContainer.style.display = 'none';
+          chatbotButton.innerHTML = '<i class="bx bx-phone"></i>';
+      }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const recordButton = document.querySelector('.record-button');
+
+  recordButton.addEventListener('mousedown', function () {
+      recordButton.classList.add('pressed');
+  });
+
+  recordButton.addEventListener('mouseup', function () {
+      recordButton.classList.remove('pressed');
+  });
+});
+
