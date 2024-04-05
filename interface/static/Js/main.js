da0ed51802867cba59b7563f932b83c98d45913a
Athukorale <athukoralaama@gmail.com>
2024-04-05 17:27:52 +0530

    Navigation Toggle Removed

--- interface/static/Js/main.js
+++ interface/static/Js/main.js
@@ -100,29 +100,6 @@ if (mediaRecorder) {
 .catch(err => console.error('Error sending audio:', err));
 }
 
-// add hovered class to selected list item
-let list = document.querySelectorAll(".navigation li");
-
-function activeLink() {
-  list.forEach((item) => {
-    item.classList.remove("hovered");
-  });
-  this.classList.add("hovered");
-}
-
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-// Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-};
-
-
 const startConvButton = document.getElementById('startConvButton');
 const endConvButton = document.getElementById('endConvButton');
 const chatContainer = document.getElementById('chatContainer');
